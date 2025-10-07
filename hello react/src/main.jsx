d0d1e837bf23cae6bb15234f0aa39d7a801bcbc0
MaximilianGcodes <maximilian.giesen@denkwerk.com>
2025-10-07 15:00:20 +0200

    added src-path to logo and 40px width

--- hello react/src/main.jsx
+++ hello react/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <main>
+        <img src={new URL('./assets/react-logo.png', import.meta.url).href} alt="logo" width={40}/>
+        <h1>Fun facts about react</h1>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 200K stars on GitHub</li>
+            <li>Is maintained by Meta</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+    </main>
 )
