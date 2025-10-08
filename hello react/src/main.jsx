40cb094a87255c9f3a46769e80e5f25fc08de2a8
MaximilianGcodes <maximilian.giesen@denkwerk.com>
2025-10-08 15:11:41 +0200

    Added App-component and restructured project

--- hello react/src/main.jsx
+++ hello react/src/main.jsx
@@ -1,13 +1,7 @@
 import { createRoot } from 'react-dom/client'
-import  Header from './components/Header.jsx'
-import  Main from './components/Main.jsx'
-import  Footer from './components/Footer.jsx'
+import  App from './components/App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
-    <>
-        <Header />
-        <Main />
-        <Footer />
-    </>
+    <App />
 )
