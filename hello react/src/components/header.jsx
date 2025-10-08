8abcac1c765114e9875d9789fb8b804d2e8af6d4
MaximilianGcodes <maximilian.giesen@denkwerk.com>
2025-10-08 14:50:59 +0200

    added some styling

--- hello react/src/components/header.jsx
+++ hello react/src/components/header.jsx
@@ -4,16 +4,17 @@ export default function Header(){
     return (
         <>
             <header>
-                <img src={new URL('../assets/react.svg', import.meta.url).href} alt="logo" width={40}/>
-                <nav>
-                    <ul className="nav-links">
-                        <li>Pricing</li>
-                        <li>About</li>
-                        <li>Contact</li>
-                    </ul>
-                </nav>
+                <div className="navbar"><img src={new URL('../assets/react.svg', import.meta.url).href} alt="logo" className="logo"/>
+                    <nav>
+                        <ul className="nav-links">
+                            <li>Pricing</li>
+                            <li>About</li>
+                            <li>Contact</li>
+                        </ul>
+                    </nav>
+                </div>
+                <h1>Fun facts about react</h1>
             </header>
-            <h1>Fun facts about react</h1>
         </>
     )
 }
--- hello react/src/main.jsx
+++ hello react/src/main.jsx
@@ -1,11 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import  Header from './components/header.jsx'
 import  List from './components/list.jsx'
+import  Footer from './components/footer.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
     <>
         <Header />
         <List />
+        <Footer />
     </>
 )
