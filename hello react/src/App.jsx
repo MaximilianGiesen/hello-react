f8b2947d8a0307409461eb3378d096bc462062b8
MaximilianGcodes <maximilian.giesen@denkwerk.com>
2025-10-08 15:44:36 +0200

    refactored header

--- hello react/src/App.jsx
+++ hello react/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import Header from './Header.jsx';
-import Main from './Main.jsx';
-import Footer from './Footer.jsx';
+import Header from './components/Header.jsx';
+import Main from './components/Main.jsx';
+import Footer from './components/Footer.jsx';
 
 export default function App() {
     return (
--- hello react/src/components/Header.jsx
+++ hello react/src/components/Header.jsx
@@ -2,19 +2,11 @@ import React from "react";
 
 export default function Header(){
     return (
-        <>
-            <header>
-                <div className="navbar"><img src={new URL('../assets/react.svg', import.meta.url).href} alt="logo" className="logo"/>
-                    <nav>
-                        <ul className="nav-links">
-                            <li>Pricing</li>
-                            <li>About</li>
-                            <li>Contact</li>
-                        </ul>
-                    </nav>
-                </div>
-                <h1>Fun facts about react</h1>
-            </header>
-        </>
+        <header>
+                <nav>
+                    <img src={new URL('../assets/react.svg', import.meta.url).href} alt="logo" className="logo"/>
+                    <h1>ReactFacts</h1>
+                </nav>
+        </header>
     )
 }
