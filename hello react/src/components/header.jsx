0b5d347615a03b8dfa3e784e7d16d2b68fd2ce7b
MaximilianGcodes <maximilian.giesen@denkwerk.com>
2025-10-08 14:37:12 +0200

    added nav-bar

--- hello react/src/components/header.jsx
+++ hello react/src/components/header.jsx
@@ -3,7 +3,16 @@ import React from "react";
 export default function Header(){
     return (
         <>
-            <img src={new URL('../assets/react.svg', import.meta.url).href} alt="logo" width={40}/>
+            <header>
+                <img src={new URL('../assets/react.svg', import.meta.url).href} alt="logo" width={40}/>
+                <nav>
+                    <ul className="nav-links">
+                        <li>Pricing</li>
+                        <li>About</li>
+                        <li>Contact</li>
+                    </ul>
+                </nav>
+            </header>
             <h1>Fun facts about react</h1>
         </>
     )
